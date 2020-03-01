e47be4b65705f8e168189d38111335fb7848c874
daniaalrababa9 <runner@fv-az50.nfk3uese4lqenflxq0vn2mr4dh.cx.internal.cloudapp.net>
2020-03-01 11:59:06 +0000

    Updates

---
+++ precache-manifest.b89feea87e4d57aeffb45d1f3bce830d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af4be9690d9b2ebbcae00db656f87d1",
+    "url": "/react-testing-and-deployment/index.html"
+  },
+  {
+    "revision": "e1e73c44eb3b05a52d79",
+    "url": "/react-testing-and-deployment/static/css/main.d594792a.chunk.css"
+  },
+  {
+    "revision": "b3fb4f6144c7505f16f9",
+    "url": "/react-testing-and-deployment/static/js/2.f599c83b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-testing-and-deployment/static/js/2.f599c83b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e73c44eb3b05a52d79",
+    "url": "/react-testing-and-deployment/static/js/main.0b3eeabf.chunk.js"
+  },
+  {
+    "revision": "913356d13eda4b220ea2",
+    "url": "/react-testing-and-deployment/static/js/runtime-main.624ed198.js"
+  }
+]);
